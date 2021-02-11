51f062d6d91abf956c7f64d964dca853d7c6effe
Oğuzhan ARSLAN <oarslan95@gmail.com>
2021-02-11 04:42:17 +0300

    Mini Basketball Project
    Include with Unity Standard Assets.

---
+++ Mini Basketball/Assets/Scripts/gameControlScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameControlScript : MonoBehaviour
+{
+    AudioSource soundeffect;
+    GameObject effect;
+    void Start()
+    {
+        soundeffect=GetComponent<AudioSource>();
+        effect=GameObject.FindGameObjectWithTag("effectTag");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.tag=="ballTag")
+        {
+            GetComponent<BoxCollider>().enabled=false;
+            soundeffect.Play();
+            effect.GetComponent<effectControlScript>().activeEff();
+        }
+    }
+}
---
+++ Mini Basketball/Assets/Scripts/playerControlScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerControlScript : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject playerCam;
+    private Rigidbody bRigidbody;
+    [SerializeField] private float distance, forcePower;
+    private bool holdingBall=true;
+    private float timer;
+    [SerializeField] private float newGameTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bRigidbody=ball.GetComponent<Rigidbody>();
+        bRigidbody.useGravity=false;
+    }
+
+    void Update () 
+    {
+        playerAction();
+        ResetGame();
+    }
+    void LateUpdate()
+    {
+        if(holdingBall) 
+        {
+            ball.transform.position=playerCam.transform.position+playerCam.transform.forward*distance;
+        }
+    }
+
+    private void playerAction()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            bRigidbody.AddForce(playerCam.transform.forward*forcePower);
+            holdingBall=false;
+            bRigidbody.useGravity=true;
+        }
+    }
+
+    private void ResetGame() {
+        if(holdingBall==false)
+        {
+            timer+=Time.deltaTime;
+           // Debug.Log(timer);
+            if(timer>=newGameTime)
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+    }
+
+    
+    
+}
